e6a90db520d483b3dcd841a46c5e7e5d32df425d
Mirko Speth <mspeth@miaplaza.com>
2016-03-05 13:18:00 +0100

    add svg output

--- Moydenskall/Enumerator.cpp
+++ Moydenskall/Enumerator.cpp
@@ -5,6 +5,7 @@
 #include "Enumerator.h"
 #include "Tools.h"
 #include <limits>
+#include <fstream>
 
 std::ostream& operator<<(std::ostream& stream, const Plane& plane) {
 	stream << "{ ";
@@ -47,6 +48,7 @@ void Enumerator::create_partition(std::vector<Plane>& partition, Plane& left) {
 		if (costs < best_costs) {
 			best_costs = costs;
 			best_sites = cen;
+			best_partition = partition;
 			std::cout << "current best value = " << costs << std::endl;
 		}
 		return;
@@ -78,4 +80,48 @@ void Enumerator::print_result() {
 	for (auto cen : best_sites) {
 		std::cout << cen.X << " / " << cen.Y << std::endl;
 	}
+	svg_output();
+}
+
+void Enumerator::svg_output() {
+	std::ofstream svgfile("result.svg");
+
+	double xmin = 99999, ymin = 99999;
+	double xmax = 0, ymax = 0;
+
+	for (auto part : best_partition) {
+		for (auto p : part) {
+			xmin = std::min(xmin, p.X);
+			xmax = std::max(xmax, p.X);
+			ymin = std::min(ymin, p.Y);
+			ymax = std::max(ymax, p.Y);
+		}
+	}
+
+	xmin -= 2; ymin -= 2; xmax += 2; ymax += 2;
+	double scale = 20;
+
+	svgfile << "<?xml version=\"1.0\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"	\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">"<<std::endl;
+	svgfile << "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"960\" height=\"700\" viewBox = \""
+		<<scale*xmin << " "<< scale*ymin << " " << scale*(xmax-xmin) << " " << scale*(ymax-ymin)
+		<<"\" preserveAspectRatio = \"xMidYMid meet\">" << std::endl;
+	//svgfile << "<rect x = \"0\" y = \"0\" width = \"500\" height = \"500\" style = \"stroke:black;stroke-width:3;fill:white\"/>" << std::endl;
+
+
+
+
+	for (int part = 0; part < best_partition.size(); ++part) {
+		auto cen = best_sites[part];
+		svgfile << "<circle cx = \"" << scale * cen.X << "\" cy = \"" << scale * cen.Y << "\" r = \""
+			<< 2 << "\" style = \"fill:green\" />" << std::endl;
+		for (auto p : best_partition[part]) {
+			svgfile << "<line x1=\"" << scale * p.X << "\" y1=\"" << scale*p.Y << "\" x2=\"" << scale * cen.X << "\" y2=\"" << scale * cen.Y
+				<< "\" style=\"stroke:#aaa; stroke-width:1; \" stroke-dasharray=\"1, 1\"/>";
+			svgfile << "<circle cx = \"" << scale * p.X << "\" cy = \"" << scale*p.Y << "\" r = \""
+				<< 1 << "\" style = \"fill:red\" />" << std::endl;
+		}
+	}
+
+
+	svgfile << "</svg>";
 }
--- Moydenskall/Enumerator.h
+++ Moydenskall/Enumerator.h
@@ -9,10 +9,12 @@ public:
 	void create_partition_of_n(std::vector<int>&, int, int);
 	void create_partition(std::vector<Plane>&, Plane&);
 	void print_result();
+	void svg_output();
 
 private:
 	Plane plane;
 	Evaluator eval;
 	double best_costs;
 	Plane best_sites;
+	std::vector<Plane> best_partition;
 };
