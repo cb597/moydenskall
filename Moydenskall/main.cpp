1918df1d33ac11c23654fdf134e017f4b068d725
Mirko Speth <mspeth@miaplaza.com>
2016-03-25 20:06:20 +0100

    Cherry pick cmdline parameter parsing

--- Moydenskall/main.cpp
+++ Moydenskall/main.cpp
@@ -6,22 +6,30 @@
 #include <iostream>
 #include "Tools.h"
 #include <string>
+#include <limits>
 
 
 int main(int argc, char* argv[]) {
 
 	double f = 50;
 	std::string instance_filename = "inst1.tsp";
+	int u = std::numeric_limits<int>::max();
 
 	if (argc >= 2) {
 		instance_filename = argv[1];
 	}
-
-	if (argc >= 3) {
-		f = std::stoi(argv[2]);
+	else {
+		throw "filename missing, usage: program filename {-f Facilities} {-u Capacity}";
 	}
-
-
+	for (int i = 2; i < argc-1; i+=2) {
+		std::string test = argv[i];
+		if (std::string(argv[i]) == "-f") {
+			f = std::stoi(argv[i+1]);
+		}
+		if (std::string(argv[i]) == "-u") {
+			u = std::stoi(argv[i+1]);
+		}
+	}
 	
 	Plane p = readfile(instance_filename);
 
