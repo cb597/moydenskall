e861187c64d80ad86368efddccd376e2731ea4f9
Mirko Speth <mspeth@miaplaza.com>
2016-03-25 21:12:28 +0100

    improved error messages in cmdl parameter parsing

--- Moydenskall/main.cpp
+++ Moydenskall/main.cpp
@@ -18,19 +18,20 @@ int main(int argc, char* argv[]) {
 	std::string instance_filename = "inst1.tsp";
 	int u = std::numeric_limits<int>::max();
 
-	if (argc >= 2) {
+	if (argc >= 2 && argc%2==0) {
 		instance_filename = argv[1];
 	}
 	else {
-		throw "filename missing, usage: program filename {-f Facilities} {-u Capacity}";
+		throw "invalid parameter count, usage: program filename {-f Facilities} {-u Capacity}";
 	}
+
 	for (int i = 2; i < argc-1; i+=2) {
 		std::string test = argv[i];
 		if (std::string(argv[i]) == "-f") {
-			f = std::stoi(argv[i+1]);
+			f = std::stoi(argv[i+1]); //fix costs
 		}
 		if (std::string(argv[i]) == "-u") {
-			u = std::stoi(argv[i+1]);
+			u = std::stoi(argv[i+1]); //capacity
 		}
 	}
 	
