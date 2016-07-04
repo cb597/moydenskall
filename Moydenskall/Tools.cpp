f1bfd92d49d09712aeabff403448ae650e6b8953
Mirko Speth <mspeth@miaplaza.com>
2016-07-04 12:26:57 +0200

    remove old svg_output

--- Moydenskall/Tools.cpp
+++ Moydenskall/Tools.cpp
@@ -101,58 +101,6 @@ Pointset readfile(std::string filename) {
 	return input_points;
 }
 
-void print_to_svg(Pointset customers, Partition partition, Pointset sites, std::string filename) {
-	std::ofstream svgfile(filename);
-
-	double xmin = 99999, ymin = 99999;
-	double xmax = 0, ymax = 0;
-
-	for (auto c : customers) {
-		xmin = std::min(xmin, c.X);
-		xmax = std::max(xmax, c.X);
-		ymin = std::min(ymin, c.Y);
-		ymax = std::max(ymax, c.Y);
-	}
-	for (auto s : sites) {
-		xmin = std::min(xmin, s.X);
-		xmax = std::max(xmax, s.X);
-		ymin = std::min(ymin, s.Y);
-		ymax = std::max(ymax, s.Y);
-	}
-
-	xmin -= 2; ymin -= 2; xmax += 2; ymax += 2;
-	double pointsize = (double)std::max((double)(xmax - xmin) / 100., (double)(ymax - ymin) / 600.);
-
-	svgfile << "<?xml version=\"1.0\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"	\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">" << std::endl;
-	svgfile << "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"900\" height=\"700\" viewBox = \""
-		<< xmin << " " << ymin << " " << (xmax - xmin) << " " << (ymax - ymin)
-		<< "\" preserveAspectRatio = \"xMidYMid meet\">" << std::endl;
-	//svgfile << "<rect x = \"0\" y = \"0\" width = \"500\" height = \"500\" style = \"stroke:black;stroke-width:3;fill:white\"/>" << std::endl;
-
-
-
-
-	for (unsigned int part = 0; part < partition.size(); ++part) {
-		auto cen = sites[part];
-		svgfile << "<circle cx = \"" << cen.X << "\" cy = \"" <<  cen.Y << "\" r = \""
-			<< pointsize << "\" style = \"fill:green\" />" << std::endl;
-		for (auto p : partition[part]) {
-			svgfile << "<line x1=\"" <<  p.X << "\" y1=\"" << p.Y << "\" x2=\"" <<  cen.X << "\" y2=\"" <<  cen.Y
-				<< "\" style=\"stroke:#aaa; stroke-width:" << pointsize << "; \" stroke-dasharray=\"0.1, 0.1\"/>";
-			svgfile << "<circle cx = \"" << p.X << "\" cy = \"" << p.Y << "\" r = \""
-				<< pointsize << "\" style = \"fill:red\" />" << std::endl;
-		}
-	}
-
-	double sum = 0;
-	for (unsigned int i = 0; i < partition.size(); ++i) {
-		sum += eucl2dist(partition[i], sites[i]);
-	}
-	svgfile << "<text x = \""<< xmin <<"\" y = \""<< ymin <<"\" fill = \"black\"  style=\"font-size:"<<3*pointsize<<"px\">" << filename << " - " << sum << "</text>\n";
-	svgfile << "</svg>";
-}
-
-
 double drand() {
 	double d = 0.;
 #ifdef _WIN32
