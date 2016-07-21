50fc2e2dfdea7701c8422ca526029feee4c3504a
Mirko Speth <mspeth@miaplaza.com>
2016-07-21 19:34:18 +0200

    some analysis stuff

---
+++ Moydenskall/win_runthemall.cmd
@@ -0,0 +1,6 @@
+.\Test\Moydenskall.exe instances\burma14.tsp -k 10 -f 0
+.\Test\Moydenskall.exe instances\latch_cluster_files\t261.tsp -k 10 -f 0
+.\Test\Moydenskall.exe instances\latch_cluster_files\v3869.tsp -k 10 -f 0
+.\Test\Moydenskall.exe instances\latch_cluster_files\I11041.tsp -k 10 -f 0
+.\Test\Moydenskall.exe instances\latch_cluster_files\L41645.tsp -k 10 -f 0
+.\Test\Moydenskall.exe instances\latch_cluster_files\i45242.tsp -k 10 -f 0
